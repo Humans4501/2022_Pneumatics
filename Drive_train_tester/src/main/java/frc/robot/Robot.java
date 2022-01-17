8748575faaa7962246cf6a14df62ee82c6441282
Humans4501 <Humans4501@users.noreply.github.com>
2022-01-17 11:27:03 -0800

    Update Robot.java

--- Drive_train_tester/src/main/java/frc/robot/Robot.java
+++ Drive_train_tester/src/main/java/frc/robot/Robot.java
@@ -1,81 +1,48 @@
+/**
+ * 1.17.22
+ * @author Finley Doolittle (4501)
+ * @author Fox Raggio (4501)
+ * Simple Falcon 500 drive trian code 
+ */
+
 package frc.robot;
-import com.ctre.phoenix.motorcontrol.can.*;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import frc.robot.constant.Constants;
 
 public class Robot extends TimedRobot {
-  //JoyStick
+  //Joy_stick
   private Joystick jleft = new Joystick(0);
   private Joystick jRight = new Joystick(1);
   //MotorControllerGroup
   MotorControllerGroup sleft; 
   MotorControllerGroup sright; 
-  //Drive trian
+  //Drive_trian_motors
   private DifferentialDrive ddrive;
   private WPI_TalonFX left0 = new WPI_TalonFX(Constants.Motors.LEFT_FORWARD);
   private WPI_TalonFX left1 = new WPI_TalonFX(Constants.Motors.LEFT_BACKWARD);
   private WPI_TalonFX right0 = new WPI_TalonFX(Constants.Motors.RIGHT_FORWARD);
   private WPI_TalonFX right1 = new WPI_TalonFX(Constants.Motors.RIGHT_BACKWARD);
-  
-  public static void wait(int ms)
-{
-    try
-    {
-        Thread.sleep(ms);
-    }
-    catch(InterruptedException ex)
-    {
-        Thread.currentThread().interrupt();
-    }
-}
 
   @Override
   public void robotInit() {
     sleft = new MotorControllerGroup(left0, left1);
     sright = new MotorControllerGroup(right0, right1);
-
-    right0.setInverted(true);
-    right1.setInverted(true);
-
-    ddrive = new DifferentialDrive(left0, right0); 
-
+    ddrive = new DifferentialDrive(left0, right0);
   }
 
   @Override
   public void teleopPeriodic() {
+
     ddrive.tankDrive(jleft.getY(), jRight.getY());
-  }
 
-  @Override
-  public void autonomousInit() {}
-
-  @Override
-  public void autonomousPeriodic() {
-
-  double time = Timer.getFPGATimestamp(); 
-
-  if (time <  120) {
-    sleft.set(0.75);
-    sright.set(0.75);
-  } else { 
-    sleft.set(0);
-    sright.set(0); 
-  }
-
-  wait(5000);
   
-  if (time < 120) {
-    sleft.set(-0.75); 
-    sright.set(-0.75);
-  } else { 
-    sleft.set(0.0); 
-    sright.set(0.0); 
   }
+}
 
-  }
-} 
+
 
