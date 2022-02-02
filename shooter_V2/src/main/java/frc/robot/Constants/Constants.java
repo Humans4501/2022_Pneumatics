27427d57ba220e777a35822ec0fc130e9e0d21b0
Humans4501 <Humans4501@users.noreply.github.com>
2022-02-01 21:56:19 -0800

    Add files via upload

---
+++ shooter_V2/src/main/java/frc/robot/Constants/Constants.java
@@ -0,0 +1,17 @@
+package frc.robot.Constants;
+
+/**
+ * Constants for the Robot.
+ * @author Fox Raggio (4501)
+ */
+
+public final class Constants {
+    /**
+     * For now, defines which CAN id is assigned to the Falcon.
+     */
+    public static final class Motors {
+        public static final int RIGHT_FALCON = 6;
+        public static final int LEFT_FALCON = 51; 
+     
+    }
+}
