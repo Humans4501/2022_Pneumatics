752d42d5c779b3c35eb286e359a1ca13c1158dfd
Humans4501 <Humans4501@users.noreply.github.com>
2022-02-03 15:03:32 -0800

    Update Robot.java

--- shooter_V2/src/main/java/frc/robot/Robot.java
+++ shooter_V2/src/main/java/frc/robot/Robot.java
@@ -1,44 +1,34 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+//2/3/22
+//4501 HUMANS
 
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import edu.wpi.first.wpilibj.Joystick;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.Constants;
 
-
 public class Robot extends TimedRobot {
-  private DifferentialDrive ddrive;
-  private static final int kJoystickPort = 0;
-  private static final int kJoystick2Port = 1; 
-  private MotorControllerGroup sleft; /** Left side control */
-  private MotorControllerGroup sright; /** Right side control */
-  private Joystick jleft = new Joystick(0);
-  private Joystick jright = new Joystick(1);
+  private XboxController Xbox = new XboxController(1);
   private WPI_TalonFX left0 = new WPI_TalonFX(Constants.Motors.LEFT_FALCON); 
-  private WPI_TalonFX right0 = new WPI_TalonFX(Constants.Motors.RIGHT_FALCON); 
+  private WPI_TalonFX right0 = new WPI_TalonFX(Constants.Motors.RIGHT_FALCON);
 
   @Override
   public void robotInit() {
-    sleft = new MotorControllerGroup(left0);
-    sright = new MotorControllerGroup(right0); 
-    ddrive = new DifferentialDrive(left0, right0);
     this.left0.setInverted(true);
-    jleft = new Joystick(kJoystickPort);
-    jright = new Joystick(kJoystick2Port);
-  }
+  } 
 
   @Override
   public void teleopPeriodic() {
-
-    ddrive.tankDrive(jleft.getY(), jright.getY());
-    
+    if(this.Xbox.getAButtonPressed()) {
+      this.left0.set(SmartDashboard.getNumber("leftSpeed", 0.0));
+      this.right0.set(SmartDashboard.getNumber("rightSpeed", 0.0));
+    }
+    if(this.Xbox.getAButtonReleased()){ 
+      this.left0.stopMotor();
+      this.right0.stopMotor();
+    }
   }
 }
