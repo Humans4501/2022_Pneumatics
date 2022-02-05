37c062ff3ee26d77c174b18682b5bae88ec3f70c
Humans4501 <Humans4501@users.noreply.github.com>
2022-02-04 17:28:48 -0800

    Update Robot.java

--- shooter_V2/src/main/java/frc/robot/Robot.java
+++ shooter_V2/src/main/java/frc/robot/Robot.java
@@ -7,35 +7,48 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.Constants;
 
 public class Robot extends TimedRobot {
   private XboxController Xbox = new XboxController(1);
-  private WPI_TalonFX left0 = new WPI_TalonFX(Constants.Motors.LEFT_FALCON); 
-  private WPI_TalonFX right0 = new WPI_TalonFX(Constants.Motors.RIGHT_FALCON);
+  private WPI_TalonFX shooter0 = new WPI_TalonFX(Constants.Motors.LEFT_FALCON); 
+  private WPI_TalonFX shooter1 = new WPI_TalonFX(Constants.Motors.RIGHT_FALCON);
 
   @Override
   public void robotInit() {
-    this.left0.setInverted(true);
+    this.shooter1.setInverted(true);
+    SmartDashboard.putNumber("leftSpeed", 0.0); 
+    SmartDashboard.putNumber("rightSpeed", 0.0);
   } 
 
   @Override
   public void teleopPeriodic() {
+//Shuffle values
+    if(this.Xbox.getXButtonPressed()) { //Change value on Shuffle board
+      this.shooter0.set(SmartDashboard.getNumber("leftSpeed", 0.0)); 
+      this.shooter1.set(SmartDashboard.getNumber("rightSpeed", 0.0)); 
+    }
+    if(this.Xbox.getXButtonReleased()){ 
+      this.shooter0.stopMotor();
+      this.shooter1.stopMotor();
+    }
+//hard set values
     if(this.Xbox.getAButtonPressed()) { //uper port speeds
-      this.left0.set(-0.71);
-      this.right0.set(-0.63);
+      this.shooter0.set(-0.71);
+      this.shooter1.set(-0.63);
     }
     if(this.Xbox.getAButtonReleased()){ 
-      this.left0.stopMotor();
-      this.right0.stopMotor();
+      this.shooter0.stopMotor();
+      this.shooter1.stopMotor();
     }
-    if(this.Xbox.getBButtonPressed()) { //lower port speeds
-      this.left0.set(-0.2);
-      this.right0.set(-0.3);
+    if(this.Xbox.getBButtonPressed()) { //lower port speeds ---> needs to be tested and tuned
+      this.shooter0.set(-0.2);
+      this.shooter1.set(-0.3);
     }
     if(this.Xbox.getBButtonReleased()) {
-      this.left0.stopMotor();
-      this.right0.stopMotor();
+      this.shooter0.stopMotor();
+      this.shooter1.stopMotor();
     }
 
   }
